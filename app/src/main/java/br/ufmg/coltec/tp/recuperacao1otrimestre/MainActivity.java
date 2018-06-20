f91c3c15a741f166745f7e63567ce44d401fc964
IcaroColtec <icaroemd18@outlook.com>
2018-06-20 15:13:11 -0300

    Método de Adcionar Pronto

--- app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/MainActivity.java
@@ -1,10 +1,17 @@
 package br.ufmg.coltec.tp.recuperacao1otrimestre;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class MainActivity extends Activity {
+
+    MainActivityAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +19,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         ListView ProdutosListView = findViewById(R.id.lista_produtos);
-        ProdutosListView.setAdapter(new MainActivityAdapter(this));
+        adapter = new MainActivityAdapter(this);
+        ProdutosListView.setAdapter(adapter);
 
 
+
+//Parte lógica do acionamento do botão de "adcionar"
+//da lista principal
+
+        Button adcionar_produto = findViewById(R.id.Btn_adcionar);
+
+        adcionar_produto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Cadastro_Produto.class );
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MainActivity.this.adapter.notifyDataSetChanged();
     }
 }
