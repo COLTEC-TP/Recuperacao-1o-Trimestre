71fbd6ea3fd2a6105e2b75f5bbd09f3e90cd0a74
IcaroColtec <icaroemd18@outlook.com>
2018-06-21 09:18:51 -0300

    Fiz a função de Remoção e a de Atualização da lista na remoção e no geral

--- app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/MainActivity.java
@@ -1,12 +1,19 @@
 package br.ufmg.coltec.tp.recuperacao1otrimestre;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -22,20 +29,48 @@ public class MainActivity extends Activity {
         adapter = new MainActivityAdapter(this);
         ProdutosListView.setAdapter(adapter);
 
+        //Criador de Dialog
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        ProdutosListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
 
-//Parte lógica do acionamento do botão de "adcionar"
-//da lista principal
-
-        Button adcionar_produto = findViewById(R.id.Btn_adcionar);
-
-        adcionar_produto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Cadastro_Produto.class );
-                startActivity(intent);
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+
+                //Toast.makeText(getBaseContext(), p, Toast.LENGTH_SHORT).show();
+
+                // Define Parâmetros para o Dialog
+                alertBuilder.setTitle("Excluir Produto?");
+
+
+                // Define o que acontece quando o usuário seleciona a opção positiva
+                alertBuilder.setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ProdutoDAO dao = ProdutoDAO.getInstance();
+                        dao.removerProduto(position);
+                        MainActivity.this.adapter.notifyDataSetChanged();
+                    }
+                });
+
+                // Define o que acontece quando o usuário seleciona a opção negativa
+                alertBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                // cria o dialog e o exibe na tela
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
+
+                return true;
             }
         });
+
 
     }
 
@@ -44,4 +79,50 @@ public class MainActivity extends Activity {
         super.onResume();
         MainActivity.this.adapter.notifyDataSetChanged();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                // inicia a activity Cadastro_Produto quando o usuário clica no botão correspondente
+                Intent intent = new Intent(MainActivity.this, Cadastro_Produto.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) { //durante a digitação do usuário filtra e seta a ListView com o resultado da busca
+                ArrayList<Produto> listaFiltrada = ProdutoDAO.getInstance().filtrarProdutos(s);
+                mostraBusca(listaFiltrada);
+                return false;
+            }
+
+
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void mostraBusca(ArrayList<Produto> produtos) {
+        ListView auxList = findViewById(R.id.lista_produtos);
+        auxList.setAdapter(new MainActivityAdapter(this, produtos));
+    }
+
 }
--- app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/ProdutoDAO.java
+++ app/src/main/java/br/ufmg/coltec/tp/recuperacao1otrimestre/ProdutoDAO.java
@@ -34,7 +34,7 @@ public class ProdutoDAO {
     /*Aqui as informações serão recuperadas do banco de dados*/
         produtos.add(new Produto("Sabão", "Limpeza", 15.54));
         produtos.add(new Produto("Escova de dentes", "Higiene", 3.7));
-
+        produtos.add(new Produto("Escova de dentes", "Higiene", 3.7));
 
     }
 
@@ -47,6 +47,18 @@ public class ProdutoDAO {
         instance.produtos.add(novoProduto);
     }
 
+
+    /**
+     * Remover um produto na lista
+     *
+     *  produto que será removido na lista
+     */
+
+    public void removerProduto(int position){
+        instance.produtos.remove(position);
+    }
+
+
     /**
      * Recupera lista completa dos produtos
      * @return ArrayList com todos os produtos cadastrados no DAO
@@ -55,6 +67,20 @@ public class ProdutoDAO {
         return instance.produtos;
     }
 
+    /**
+     * @param busca expressão buscada oelo usuário
+     * @return ArrayList apenas cuja categoria ou nome correspondem ao buscado pelo usuário
+     */
+    public ArrayList<Produto> filtrarProdutos(String busca) {
+        ArrayList<Produto> filtrados = new ArrayList<>();
+
+        for(int i=0;i<produtos.size();i++){
+            if(produtos.get(i).getNome().toLowerCase().contains(busca.toLowerCase()) || produtos.get(i).getCategoria().toLowerCase().contains(busca.toLowerCase())){
+                filtrados.add(produtos.get(i));
+            }
+        }
+        return filtrados;
+    }
 
 
 }
